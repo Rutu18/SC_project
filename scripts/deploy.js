06a9ab9fe17c5f39b8e69a5af95245b963a48d2a
Rutvika S <rutvikas@Rutvikas-MacBook-Pro.local>
2022-10-27 15:01:01 -0400

    build organization of project.

---
+++ scripts/deploy.js
@@ -0,0 +1,21 @@
+// scripts/deploy.js
+// const hre = require("hardhat");
+
+async function main() {
+  // We get the NFT_AC contract to deploy
+  var name = 'NFT-CapAC';
+  var symbol = 'CapAC';
+  const NFT_CapAC = await ethers.getContractFactory('NFT_CapAC');
+  console.log('Deploying NFT_CapAC...');
+  const nft_ac = await NFT_CapAC.deploy(name,symbol);
+  await nft_ac.deployed();
+
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
